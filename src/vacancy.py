da5ea3933b050c5c39d6ee87526b7118b17f35e8
Kostin Igor <grizzlygit777@gmail.com>
2024-07-05 14:42:09 +0300

    Vacancy and BaseVacancy refactoring

--- src/vacancy.py
+++ src/vacancy.py
@@ -7,41 +7,27 @@ class Vacancy(BaseVacancy):
     link: str
     pay: int
     description: str
-    requirements: str
 
-    def __init__(self, name, link, description, requirements, pay):
+    def __init__(self, name, link, description, pay):
         self.name = name
         self.link = link
         self.pay = pay
         self.description = description
-        self.requirements = requirements
-
-    @property
-    def pay(self):
-        return self._pay
-
-    @pay.setter
-    def pay(self, payment):
-        if payment is None:
-            self._pay = "Зарплата не указана"
-        else:
-            self._pay = payment
 
     @classmethod
-    def new_vacancy(cls, name, link, description, requirements, pay):
+    def new_vacancy(cls, name, link, pay, description):
         return cls(
             name,
             link,
-            description,
-            requirements,
             pay,
+            description,
         )
 
     def __repr__(self):
-        return f"{type(self).__name__}({self.name}, {self.link}, {self.description}, {self.requirements}, {self.pay})"
+        return f"{type(self).__name__}({self.name}, {self.link}, {self.description}, {self.pay})"
 
     def __str__(self):
-        return f"{self.name},\n {self.link},\n {self.description},\n {self.requirements},\n {self.pay}\n"
+        return f"{self.name},\n {self.link},\n {self.description},\n {self.pay}\n"
 
     def __eq__(self, other) -> bool:
         return self.pay == other.pay
